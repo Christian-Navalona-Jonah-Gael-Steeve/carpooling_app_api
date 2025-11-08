ac8fb82187895ddb149ea80f6345a1d8bd68c326
Steeve Rajaonarisoa <tiavinarilala@gmail.com>
2025-11-08 14:09:50 +0300

    Recticaition erreur

--- src/main/java/mbds/car/pooling/entities/User.java
+++ src/main/java/mbds/car/pooling/entities/User.java
@@ -46,4 +46,23 @@ public class User {
     @Enumerated(EnumType.STRING)
     private AccountStatus status = AccountStatus.PENDING;
 
+    public User(String uid, String email, String firstName, String lastName,
+                String phoneNumber, String cinNumber, String gender,
+                String justificatifUrl, String city, String codePostal, String address,
+                List<UserRole> roles, AccountStatus status) {
+        this.uid = uid;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.cinNumber = cinNumber;
+        this.gender = gender;
+        this.justificatifUrl = justificatifUrl;
+        this.city = city;
+        this.codePostal = codePostal;
+        this.address = address;
+        this.roles = roles;
+        this.status = status;
+    }
+
 }
